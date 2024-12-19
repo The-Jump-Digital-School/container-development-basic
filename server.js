4dbd4f5457dbebb0774213c66a22927286b283c7
James Sherry <james.m.sherry@googlemail.com>
2024-12-19 16:29:28 +0000

    changed message

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello C11 world!\n');
+	res.send('Hello world!\n');
 });
 
 app.listen(PORT, HOST);
